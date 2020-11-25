53695a4da7c3dd72ca806c5c3bdcf4c2d7fcb104
Viktor Nagy <viktor.nagy1995@gmail.com>
2020-11-25 11:00:10 +0100

    feat: scheduler + target struct

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
 extern crate ncurses;
+extern crate job_scheduler;
+use job_scheduler::{JobScheduler, Job};
+// use std::time::Duration;
 
 mod data;
 
 use ncurses::*;
 use rand::Rng;
 
-static WINDOW_HEIGHT: i32 = 3;
-static WINDOW_WIDTH: i32 = 10;
+// static WINDOW_HEIGHT: i32 = 3;
+// static WINDOW_WIDTH: i32 = 10;
+static CORNER_PADDING: i32 = 5;
+
+struct Target {
+    x: i32,
+    y: i32,
+    remaining: i32,
+    word: String
+}
 
 fn main()
 {
@@ -15,32 +26,41 @@ fn main()
 
     let mut rng = rand::thread_rng();
 
+    let mut targets: Vec<Target> = Vec::new();
+
     /* Get the screen bounds. */
     let mut max_x = 0;
     let mut max_y = 0;
     getmaxyx(stdscr(), &mut max_y, &mut max_x);
 
-    for word in &dictionary {
-        let x = rng.gen_range(1, max_x - 1);
-        let y = rng.gen_range(1, max_y - 1);
-        spawn_word(x, y, &word);
-    }
+    let mut sched = JobScheduler::new();
+    sched.add(Job::new("* * * * * *".parse().unwrap(), || {
+        let choice = dictionary[rng.gen_range(0, dictionary.len())];
+        let mut target = Target {
+            x: rng.gen_range(CORNER_PADDING, max_x - CORNER_PADDING),
+            y: rng.gen_range(CORNER_PADDING, max_y - CORNER_PADDING),
+            word: String::from(choice),
+            remaining: choice.len() as i32
+        };
+        spawn_word(target.x, target.y, target.word.clone().as_str());
+        targets.push(target);
+    }));
+
     /* Start in the center. */
-    let mut start_y = (max_y - WINDOW_HEIGHT) / 2;
-    let mut start_x = (max_x - WINDOW_WIDTH) / 2;
+    // let mut start_y = (max_y - WINDOW_HEIGHT) / 2;
+    // let mut start_x = (max_x - WINDOW_WIDTH) / 2;
 
     let mut ch = getch();
     while ch != KEY_F(1)
     {
-
+        sched.tick();
         ch = getch();
     }
-
     endwin();
 }
 
-fn spawn_word(x: i32, y: i32, word: &str ){
-    mvprintw(x, y, &word);
+fn spawn_word(x: i32, y: i32, word: &str){
+    mvprintw(x, y, word);
 }
 
 fn setup(){
